325072cb214ae551d71a6a08085a360198d20caa
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-02-27 22:50:09 +0900

    feat: 크루 위키

--- js/index.js
+++ js/index.js
@@ -17,3 +17,40 @@ console.log(
 );
 
 console.log("알맞은 스크립트를 작성하세요");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("comment");
+  const textarea = form.querySelector("textarea");
+  const commentList = document.querySelector(".comment-list");
+  const submitButton = form.querySelector("button[type='button']");
+  const resetButton = form.querySelector("button[type='reset']");
+
+  submitButton.addEventListener("click", () => {
+    const commentText = textarea.value.trim();
+
+    if (commentText === "") {
+      alert("댓글을 입력해주세요.");
+      return;
+    }
+
+    const commentItem = document.createElement("li");
+    commentItem.innerHTML = `
+      <div class="comment-item">
+        <div class="comment-author">
+          <img src="./images/comment-author-icon.png" alt="사용자 프로필 이미지"/>
+          <span>방문자</span>
+        </div>
+        <div class="comment-content">
+          ${commentText}
+        </div>
+      </div>
+    `;
+
+    commentList.appendChild(commentItem);
+    textarea.value = "";
+  });
+
+  resetButton.addEventListener("click", () => {
+    textarea.value = "";
+  });
+});
